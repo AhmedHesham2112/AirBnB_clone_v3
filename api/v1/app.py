60c68e7ad27bd062bdfefd0e421fe4834dacc61d
AhmedHesham2112 <AhmedHesham2112>
2024-05-25 15:37:06 +0300

    api1

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,11 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(os.getenv('HBNB_API_PORT', '5000'))
-
 
 @app.teardown_appcontext
 def teardown_app(code):
@@ -21,4 +19,6 @@ def teardown_app(code):
     storage.close()
 
 if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
