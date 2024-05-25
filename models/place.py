b43c2af99b47de98fd49949996e943ee75ab60b4
root <root@DESKTOP-LP5R8B0>
2024-05-25 15:56:14 +0300

    task4

--- models/place.py
+++ models/place.py
@@ -9,11 +9,13 @@ from sqlalchemy.orm import relationship
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60, collation='latin1_swedish_ci'), 
+                          Column('place_id',
+                                 String(60, collation='latin1_swedish_ci'),
                                  ForeignKey('places.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
-                          Column('amenity_id', String(60, collation='latin1_swedish_ci'),
+                          Column('amenity_id',
+                                 String(60, collation='latin1_swedish_ci'),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True))
