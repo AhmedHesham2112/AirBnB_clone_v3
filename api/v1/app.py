60085fcf414c81bf59cfb9a7faf0d951b2d16ef9
root <root@DESKTOP-LP5R8B0>
2024-05-25 14:18:28 +0300

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+    This module contains variables and methods used to connect to API
+'''
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
+
+
+@app.teardown_appcontext
+def teardown_app(code):
+    '''
+        Handles teardown
+    '''
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=host, port=port, threaded=True)
